5b44acaa9ffb10830afe5a05570ce8a4600102a6
Leonardo Gago <ljgago@gmail.com>
2024-12-03 14:21:33 -0300

    rust(2024): day 3 done

--- 2024/rust/src/day03/bin/part1.rs
+++ 2024/rust/src/day03/bin/part1.rs
@@ -1,7 +1,32 @@
 //! # Advent of Code - Day 3 - Part One
 
-pub fn part1() -> usize {
-    return 0;
+use regex::Regex;
+
+pub fn part1(input: &str) -> i32 {
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    let instructions: Vec<&str> = re.find_iter(input).map(|m| m.as_str()).collect();
+
+    solve_instructions(instructions)
+}
+
+pub fn solve_instructions(instructions: Vec<&str>) -> i32 {
+    let mut result = 0;
+
+    for inst in instructions {
+        let inst = inst.trim_matches(['m', 'u', 'l', '(', ')']);
+
+        match inst.split(",").collect::<Vec<&str>>()[..] {
+            [x, y] => {
+                let x = x.parse::<i32>().unwrap();
+                let y = y.parse::<i32>().unwrap();
+
+                result += x * y;
+            }
+            _ => continue
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -10,6 +35,7 @@ mod day03 {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 0);
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(part1(input), 161);
     }
 }
--- 2024/rust/src/day03/bin/part2.rs
+++ 2024/rust/src/day03/bin/part2.rs
@@ -1,7 +1,29 @@
 //! # Advent of Code - Day 3 - Part Two
 
-pub fn part2() -> usize {
-    return 0;
+use regex::Regex;
+
+use crate::part1::solve_instructions;
+
+pub fn part2(input: &str) -> i32 {
+    let re = Regex::new(r"(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))").unwrap();
+    let instructions: Vec<&str> = re.find_iter(input).map(|m| m.as_str()).collect();
+
+    let mut enabled_instructions: Vec<&str> = Vec::new();
+    let mut is_enabled = true;
+
+    for inst in instructions {
+        match inst {
+            "don't()" => is_enabled = false,
+            "do()" => is_enabled = true,
+            _ => {
+                if is_enabled {
+                    enabled_instructions.push(inst);
+                }
+            }
+        }
+    }
+
+    solve_instructions(enabled_instructions)
 }
 
 #[cfg(test)]
@@ -10,6 +32,7 @@ mod day03 {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 0);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(part2(input), 48);
     }
 }
